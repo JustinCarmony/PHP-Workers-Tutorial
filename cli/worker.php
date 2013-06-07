52710d3f0f8c940dabddf10a693aaa0b5207299b
Justin Carmony <justin@justincarmony.com>
2013-06-07 12:17:20 +0200

    Adding some comments to the workers

--- cli/worker.php
+++ cli/worker.php
@@ -1,47 +1,71 @@
 <?php
 
-// Include Autoloader
-
 // Make sure our curent working directory is where the worker is being executed
 chdir(__DIR__);
 
+// Include Autoloader
 require_once '../vendor/autoload.php';
 
+// Include the Config to get the API Key
 require_once '../config.php';
 
+/**
+ * Our DemoWorker class will handle all the functionality of our worker.
+ * At the end of this file it will create an instance of this class
+ * and run Setup() and Run()
+ */
 class DemoWorker
 {
+    // Simple Worker ID
     public $worker_id;
+
+    // Unique, randomly generated Hash for each worker instance
     public $worker_hash;
+
+    // The worker version supplied from Redis, if changed the worker will exit.
     public $worker_version;
 
+    // When the worker starts
     public $start_time;
+
+    // When the worker should end
     public $end_time;
+
+    // How long of a time limit the worker will use
     public $time_limit;
+
+    // As long as true, worker will continue to work. If false, the worker will
+    // stop it's loop and run Cleanup() and exit.
     public $run = true;
+
+    // A current status of the worker for reporting info.
     public $status = 'init';
 
     // Other Details for Reporting
     public $last_word;
     public $last_def;
 
+    // Variables for Pheanstalk Jobs
     /**
      * @var Pheanstalk_Job
      */
     public $job;
     public $job_details;
 
+    // Redis client
     /**
      * @var \Predis\Client
      */
     public $redis;
 
+    // Beanstalk Client
     /**
      * @var Pheanstalk_Pheanstalk
      */
     public $beanstalk;
 
 
+    // Construct our worker, only basic settings.
     public function __construct($worker_id)
     {
         if(!is_numeric($worker_id) || $worker_id <= 0)
@@ -72,6 +96,7 @@ class DemoWorker
         echo "[".date("H:i:s")."] ".$txt."\n";
     }
 
+    // Setup the worker's connections & data from Redis
     public function Setup()
     {
         // Set Timeout Limit to 0 so we won't get killed for running too long
@@ -92,28 +117,47 @@ class DemoWorker
         $this->Log("Setup Complete");
     }
 
+    // Our main run loop
     public function Run()
     {
         $this->Log("Beginning to Run");
+
+        // We want to catch any errors in case something goes really wrong and cleanly exit
         try
         {
+            /************* MAGIC IS HERE **************
+             * This is where the magic happens
+             * The worker will loop forever while $this->run
+             * is true.
+             ******************************************/
             while($this->run)
             {
+                // Report our current status
                 $this->status = 'waiting';
                 $this->Report();
 
+                // Listen to a beanstalk tube
                 $job = $this->beanstalk
+                    // Watch the system queue for anything important
                     ->watch('system')
+                    // Watch main queue for jobs
                     ->watch('queue')
+                    // Ignore the "default" queue, jobs coming from
+                    // here are a bug.
                     ->ignore('default')
+                    // Listen up to 10 seconds for a job.
                     ->reserve(10);
 
+                // When a job exists, process the job.
                 if($job)
                 {
                     $this->ProcessJob($job);
                 }
 
+                // Echo a "." so we know the worker isn't frozen in the logs.
                 echo ".";
+
+                // Check the status of the worker to see if we need to stop running.
                 $this->CheckStatus();
             }
 
@@ -132,6 +176,12 @@ class DemoWorker
     }
 
     /**
+     * Process a job.
+     *
+     * Each individual job will run through here. We could use some fun reflection
+     * to auto detect which function to use, but we'll be explicit for the sake
+     * of the demo.
+     *
      * @param $job Pheanstalk_Job
      */
     public function ProcessJob($job)
@@ -180,42 +230,41 @@ class DemoWorker
 
         try
         {
+            $url = 'http://www.dictionaryapi.com/api/v1/references/collegiate/xml/'
+                    .urlencode($word).'?key='.DICTIONARY_API_KEY;
 
+            $contents = @file_get_contents($url);
 
-
-        $url = 'http://www.dictionaryapi.com/api/v1/references/collegiate/xml/'
-                .urlencode($word).'?key='.DICTIONARY_API_KEY;
-
-        $contents = @file_get_contents($url);
-
-        /*
-         * I really don't know how to manipulate very complex XML easily in PHP,
-         * so please ignore the hideous hacks that you are about to see.
-         */
-        $xml = new SimpleXMLElement($contents);
-        $results = $xml->xpath('/entry_list/entry[1]//dt');
-        $return = '';
-        foreach($results as $node)
-        {
-            /* @var $node SimpleXMLElement */
-            $string = $node->asXML();
-
-            // Please ignore the developer behind the curtain
-            $string = str_replace('<dt>:', '', $string);
-            $string = str_replace(':</dt>', '', $string);
-            $def = trim(strip_tags($string));
-
-            // Return the longest definition
-            if(strlen($def) > strlen($return))
+            /*
+             * I really don't know how to manipulate very complex XML easily in PHP,
+             * so please ignore the hideous hacks that you are about to see.
+             */
+            $xml = new SimpleXMLElement($contents);
+            $results = $xml->xpath('/entry_list/entry[1]//dt');
+            $return = '';
+            foreach($results as $node)
             {
-                $return = $def;
+                /* @var $node SimpleXMLElement */
+                $string = $node->asXML();
+
+                // Please ignore the developer behind the curtain
+                $string = str_replace('<dt>:', '', $string);
+                $string = str_replace(':</dt>', '', $string);
+                $def = trim(strip_tags($string));
+
+                // Return the longest definition
+                if(strlen($def) > strlen($return))
+                {
+                    $return = $def;
+                }
             }
-        }
 
         } catch(Exception $ex)
         {
             $def = "$word failed: ".$ex->getMessage();
         }
+
+        // Okay, you can start paying attention again now.
 
         $json = json_encode(array(
             'word' => $word
@@ -233,7 +282,8 @@ class DemoWorker
 
         $this->beanstalk->delete($this->job);
 
-        usleep(2000000); // Wait 2 seconds so we don't overload the API
+        // Wait 2 seconds so we don't overload the API
+        usleep(2000000);
     }
 
     public function CheckStatus()
