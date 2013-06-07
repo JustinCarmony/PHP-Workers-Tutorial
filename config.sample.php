46944cf431d0005a9df0d0ffde33c17a2e8e4435
Justin Carmony <justin@justincarmony.com>
2013-06-07 11:47:51 +0200

    Some more changes to README to add instructions and set sample config file

---
+++ config.sample.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: jcarmony
+ * Date: 5/1/13
+ * Time: 11:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+// Get an API Key at http://www.dictionaryapi.com/
+// or if you are lazy you can just use mine.
+define("DICTIONARY_API_KEY", '06b09080-d90a-452c-a6cd-806e9f8d8272');
